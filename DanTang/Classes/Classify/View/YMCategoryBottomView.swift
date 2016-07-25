cc6b086c89b0140f3df1ec541938b9fe6aa923f7
hrscy <15399070815@163.com>
2016-07-26 07:54:35 +0800

    优化分类页面的代码

--- DanTang/Classes/Classify/View/YMCategoryBottomView.swift
+++ DanTang/Classes/Classify/View/YMCategoryBottomView.swift
@@ -38,29 +38,17 @@ class YMCategoryBottomView: UIView {
         let topGroups = outGroups[0]
         let bottomGroups = outGroups[1]
         
-        let buttonW: CGFloat = SCREENW / 4
-        let buttonH: CGFloat = buttonW
-        let styleLabelH: CGFloat = 40
         // 风格
         let topView = UIView()
         topView.width = SCREENW
         topView.backgroundColor = UIColor.whiteColor()
         addSubview(topView)
+        let styleLabel = setupLabel("风格")
         topView.addSubview(styleLabel)
         
         for index in 0..<topGroups.count {
             let group = topGroups[index] as! YMGroup
-            let button = YMVerticalButton()
-            button.width = buttonW
-            button.height = buttonH
-            button.x = buttonW * CGFloat(index % 4)
-            button.y = buttonH * CGFloat(index / 4) + styleLabelH
-            button.tag = group.id!
-            button.addTarget(self, action: #selector(groupButonClick(_:)), forControlEvents: .TouchUpInside)
-            button.titleLabel?.font = UIFont.systemFontOfSize(15)
-            button.setTitleColor(YMColor(0, g: 0, b: 0, a: 0.6), forState: .Normal)
-            button.kf_setImageWithURL(NSURL(string: group.icon_url!)!, forState: .Normal)
-            button.setTitle(group.name, forState: .Normal)
+            let button = setupButton(index, group: group)
             topView.addSubview(button)
             if index == topGroups.count - 1 {
                 topView.height = CGRectGetMaxY(button.frame) + kMargin
@@ -69,28 +57,16 @@ class YMCategoryBottomView: UIView {
         
         // 品类
         let bottomView = UIView()
-        bottomView.width = topView.width
+        bottomView.width = SCREENW
         bottomView.y = CGRectGetMaxY(topView.frame) + kMargin
         bottomView.backgroundColor = UIColor.whiteColor()
         addSubview(bottomView)
+        let categoryLabel = setupLabel("品类")
         bottomView.addSubview(categoryLabel)
         
         for index in 0..<bottomGroups.count {
             let group = bottomGroups[index] as! YMGroup
-            let button = YMVerticalButton()
-            button.width = buttonW
-            button.height = buttonH
-            button.x = buttonW * CGFloat(index % 4)
-            button.y = buttonH * CGFloat(index / 4) + styleLabelH
-            if index >= 3 {
-                button.y = buttonH * CGFloat(index / 4) + styleLabelH + kMargin
-            }
-            button.tag = group.id!
-            button.addTarget(self, action: #selector(groupButonClick(_:)), forControlEvents: .TouchUpInside)
-            button.titleLabel?.font = UIFont.systemFontOfSize(15)
-            button.setTitleColor(YMColor(0, g: 0, b: 0, a: 0.6), forState: .Normal)
-            button.kf_setImageWithURL(NSURL(string: group.icon_url!)!, forState: .Normal)
-            button.setTitle(group.name, forState: .Normal)
+            let button = setupButton(index, group: group)
             bottomView.addSubview(button)
             if index == bottomGroups.count - 1 {
                 bottomView.height = CGRectGetMaxY(button.frame) + kMargin
@@ -98,23 +74,37 @@ class YMCategoryBottomView: UIView {
         }
     }
     
+    private func setupButton(index: Int, group: YMGroup) -> YMVerticalButton{
+        let buttonW: CGFloat = SCREENW / 4
+        let buttonH: CGFloat = buttonW
+        let styleLabelH: CGFloat = 40
+        
+        let button = YMVerticalButton()
+        button.width = buttonW
+        button.height = buttonH
+        button.x = buttonW * CGFloat(index % 4)
+        button.y = buttonH * CGFloat(index / 4) + styleLabelH
+        if index > 3 {
+            button.y = buttonH * CGFloat(index / 4) + styleLabelH + kMargin
+        }
+        button.tag = group.id!
+        button.addTarget(self, action: #selector(groupButonClick(_:)), forControlEvents: .TouchUpInside)
+        button.titleLabel?.font = UIFont.systemFontOfSize(15)
+        button.setTitleColor(YMColor(0, g: 0, b: 0, a: 0.6), forState: .Normal)
+        button.kf_setImageWithURL(NSURL(string: group.icon_url!)!, forState: .Normal)
+        button.setTitle(group.name, forState: .Normal)
+        return button
+    }
+    
     func groupButonClick(button: UIButton) {
         delegate?.bottomViewButtonDidClicked(button)
     }
     
-    private lazy var styleLabel: UILabel = {
+    private func setupLabel(title: String) -> UILabel {
         let styleLabel = UILabel(frame: CGRectMake(10, 0, SCREENW - 10, 40))
-        styleLabel.text = "风格"
+        styleLabel.text = title
         styleLabel.textColor = YMColor(0, g: 0, b: 0, a: 0.6)
         styleLabel.font = UIFont.systemFontOfSize(16)
         return styleLabel
-    }()
-    
-    private lazy var categoryLabel: UILabel = {
-        let categoryLabel = UILabel(frame: CGRectMake(10, 0, SCREENW - 10, 40))
-        categoryLabel.text = "品类"
-        categoryLabel.textColor = YMColor(0, g: 0, b: 0, a: 0.6)
-        categoryLabel.font = UIFont.systemFontOfSize(16)
-        return categoryLabel
-    }()
+    }
 }
